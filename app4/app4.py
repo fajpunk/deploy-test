7f74e4a939e7bbb6c47ae836dd5c7804284dcbd4
Dan Fuchs <danfuchs@slac.stanford.edu>
2026-08-24 22:50:34 -0500

    We don't need 4 apps to prove a point

--- app4/app4.py
+++
@@ -1,3 +0,0 @@
-# This doesn't actually matter, we just want to see what happens in the GitHub
-# UI with different kinds of GitHub Actions workflows for deployment. We're not
-# actually going to deploy anything anywhere.
